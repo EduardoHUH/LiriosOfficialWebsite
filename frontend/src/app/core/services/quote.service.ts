36fa8c7d563836ae5ae2521e6e418299cffb8a53
EduardoHUH <marcialeduardo8988@gmail.com>
2026-05-01 17:31:05 -0600

    Fix de los environments

--- frontend/src/app/core/services/quote.service.ts
+++ frontend/src/app/core/services/quote.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { timeout } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { API_BASE_URL } from './config';
 import {
   QuoteCreatePayload,
